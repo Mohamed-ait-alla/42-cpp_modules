e5869494d8f68d18dc29c58293788906a97c4f5a
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-07-31 11:52:39 +0100

    feat(cpp_00): complete ex00 - Megaphone program

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/*
+* Author: Mohamed Ait Alla
+* Description: This program takes command line arguments and converts them to uppercase.
+* If no arguments are specified, it shows a default message.
+* Usage: ./megaphone [arg1] [arg2] [...] [argn]
+*/
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac != 1)
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j] != '\0'; j++)
+				std::cout << (char)toupper(av[i][j]);
+			std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
+}
