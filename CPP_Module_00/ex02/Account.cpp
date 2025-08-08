a060727d6ea05093fb31bae77e2ed8cb0e603130
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-08 19:46:08 +0100

    feat(CPP_00): add destructor and _displayTimestamp implementation

--- CPP_Module_00/ex02/Account.cpp
+++ CPP_Module_00/ex02/Account.cpp
@@ -1,5 +1,6 @@
-#include "Account.hpp"
 #include <iostream>
+#include <ctime>
+#include "Account.hpp"
 
 // init static variables
 int Account::_nbAccounts = 0;
@@ -7,6 +8,7 @@ int Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
+// constructor of the Account class
 Account::Account(int initial_deposit)
 {
 	_accountIndex = _nbAccounts;
@@ -20,4 +22,31 @@ Account::Account(int initial_deposit)
 	std::cout << "index:" << _accountIndex
 			  << ";amount:" << _amount
 			  << ";created" << std::endl;
+}
+
+// destructor of the Account class
+Account::~Account( void )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";closed" << std::endl;
+}
+
+std::string getCurrentTime()
+{
+	char	formattedDateTime[80];
+	time_t	timestamp;
+	struct tm	*dateTime;
+	
+	time(&timestamp);
+	dateTime = localtime(&timestamp);
+	strftime(formattedDateTime, sizeof(formattedDateTime), "[%Y%m%d_%H%M%S] ", dateTime);
+	return (formattedDateTime);
+}
+
+// function that displays the current time
+void	Account::_displayTimestamp( void )
+{
+	std::cout << getCurrentTime();
 }
