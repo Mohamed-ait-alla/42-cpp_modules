61e1009a296c930a842d4ac540f318592ff166e1
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-08 12:38:35 +0100

    feat(CPP_00): implement the constructor

---
+++ CPP_Module_00/ex02/Account.cpp
@@ -0,0 +1,23 @@
+#include "Account.hpp"
+#include <iostream>
+
+// init static variables
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_nbAccounts ++;
+	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";created" << std::endl;
+}
