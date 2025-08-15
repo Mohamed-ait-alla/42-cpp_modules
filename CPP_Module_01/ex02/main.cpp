8dfe57b619616d87936979bb4ed0f80166d855be
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-15 21:20:28 +0100

    feat(CPP_01): complete ex02 - References concept

--- CPP_Module_01/ex02/main.cpp
+++ CPP_Module_01/ex02/main.cpp
@@ -1,19 +1,35 @@
 /*
  * @author:
+ *			Mohamed Ait Alla
  *
- *
- * 
  * @description:
- * 
- * 
- * 
+ * 				The main goal of this exercise is to understand References, how they work,
+ * 				and how they differ from pointers.	
  * 
  * @usage:
- * 
+ * 			1. Compile:	make
+ * 			2. Run:		./refConcept
 */
+
+#include <iostream>
 
 int main()
 {
+	// Declaration and initialization of variables
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	// print memory addresses
+	std::cout << "--------- SHOW ADDRESSES ---------\n\n";
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
 	
+	// print values
+	std::cout << "\n--------- SHOW VALUES ---------\n\n";
+	std::cout << "The value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 	return (0);
 }
