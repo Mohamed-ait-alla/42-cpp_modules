df0f656419cd33927c13506c61c23a008a7de9fb
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-16 14:55:35 +0100

    feat(CPP_01): complete ex03 - learn when you should use a reference or a pointer

--- CPP_Module_01/ex03/main.cpp
+++ CPP_Module_01/ex03/main.cpp
@@ -18,12 +18,15 @@
 #include "HumanA.hpp"
 #include "HumanB.hpp"
 
+// main function for tests
 int main()
 {
 	{
 		Weapon club = Weapon("crude spiked club");
 
 		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("AK47");
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
@@ -33,6 +36,8 @@ int main()
 
 		HumanB jim("Jim");
 		jim.setWeapon(club);
+		jim.attack();
+		club.setType("AK47");
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
