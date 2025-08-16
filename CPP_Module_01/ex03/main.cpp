edaf2a866acf0c69e0919a93ae959272d34794fa
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-16 14:50:34 +0100

    feat(CPP_01): complete ex03 - learn when you should use a reference or a pointer

--- CPP_Module_01/ex03/main.cpp
+++ CPP_Module_01/ex03/main.cpp
@@ -1,18 +1,18 @@
 /*
  * @author:
- *			TODO:...
- * 
- * 
+ *			Mohamed Ait Alla
  * 
  * @description:
- * 			TODO:...
- * 
- * 
- * 
+ * 				The goal is to implement two classes (HumanA and HumanB) that both use a Weapon class,
+ * 				but with different behaviors:
+ * 				  - HumanA is always armed and takes its Weapon by reference in the constructor.
+ * 				  - HumanB may start unarmed and can be assigned a Weapon later via a setter.
+ * 				This exercise focuses on references vs pointers, and illustrates when each should be used.
  * 
  * @usage:
- * 			TODO:...
- * 
+ * 			1. Compile:		make
+ * 			2. Run:			./weapon
+ * 			3. full clean:	make fclean
 */
 
 #include "HumanA.hpp"
