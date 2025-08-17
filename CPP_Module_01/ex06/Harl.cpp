89b8ea7714b40bc2e34838a904cfb9606f7a1284
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-17 14:59:59 +0100

    feat(CPP_01): complete ex06 - a complaint system

---
+++ CPP_Module_01/ex06/Harl.cpp
@@ -0,0 +1,67 @@
+#include "Harl.hpp"
+
+// Private member functions definitions
+void	Harl::debug( void )
+{
+	std::cout << BLUE "[ DEBUG ]" RESET << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+			  << std::endl
+			  << std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout << GREEN "[ INFO ]" RESET << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+			  << std::endl
+			  << std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout << YELLOW "[ WARNING ]" RESET << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+			  << std::endl
+			  << std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout << RED "[ ERROR ]" RESET << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now."
+			  << std::endl
+			  << std::endl;
+}
+
+// Public member function definition
+void	Harl::complain( std::string level )
+{
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int i;
+	
+	// Search for the specified level
+	for (i = 0;i < 4; i++)
+	{
+		if (level == levels[i])
+			break;
+	}
+	// Print the messages depending on the specified level
+	switch (i)
+	{
+	case DEBUG:
+		this->debug();
+		// fall through
+	case INFO:
+		this->info();
+		// fall through
+	case WARNING:
+		this->warning();
+		// fall through
+	case ERROR:
+		this->error();
+		break;
+	default:
+		std::cout << CYAN "[ Probably complaining about insignificant problems ]" RESET << std::endl;
+		break;
+	}
+}
--- CPP_Module_01/ex06/main.cpp
+++ CPP_Module_01/ex06/main.cpp
@@ -1,19 +1,34 @@
 /*
  * @author:
- *
+ *			Mohamed Ait Alla
  * 
  * @description:
+ * 				This exercise extends the previous Harl class by implementing a complaint
+ * 				filtering system. The program receives a complaint level and executes only
+ * 				the complaints that are equal to or above that level.
+ * 				The available levels are: "DEBUG", "INFO", "WARNING", "ERROR"
  * 
- * 
- * 
- * 
+ * 				This exercise focuses on practicing switch-case statements with fall-through
+ * 				behavior.
  * 
  * @usage:
- * 
+ * 			1. Compile:		make
+ * 			2. Run:			./harlFilter [level]
+ * 			3. Clean up:	make clean
+ * 			4. Full clean:	make fclean
 */
+
+#include "Harl.hpp"
 
 int main(int ac, char **av)
 {
-
+	Harl	harl;
+	
+	// Invalid args
+	if (ac != 2)
+		return (std::cerr << CYAN "Usage: ./harlFilter [level]" RESET "\n", 1);
+	
+	// Process the entred level
+	harl.complain(av[1]);
 	return (0);
 }
