3b141a247c099cdcfedef72df33042c05e4cf8fa
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-17 12:02:42 +0100

    feat(CPP_01): complete ex05 - Harl complaints

--- CPP_Module_01/ex05/main.cpp
+++ CPP_Module_01/ex05/main.cpp
@@ -1,16 +1,20 @@
 /*
  * @author:
- *			TODO:...
- * 
+ *			Mohamed Ait Alla
  * 
  * @description:
- * 				TODO:...
+ * 				The goal is to design a simple class (Harl) that simulates different levels
+ * 				of complaints. Each complaint level ("DEBUG", "INFO", "WARNING", "ERROR")
+ * 				corresponds to a specific private member function. The task is to map input
+ * 				strings to these member functions, triggering the appropriate complaint level.
  * 
- * 
- * 
+ * 				This exercise introduces the concept of function pointers to member functions
+ * 				and how they can be used to implement dynamic behavior.
  * 
  * @usage:
- * 			TODO:...
+ * 			1. Compile:		make
+ * 			2. Run:			./harl
+ * 			3. Full clean:	make fclean
 */
 
 #include "Harl.hpp"
