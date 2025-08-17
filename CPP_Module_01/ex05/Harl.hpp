d02b91b7945e511c4738770b14b96b0c041276dc
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-08-17 11:39:15 +0100

    feat(CPP_01): ex05 - implement Harl class functionality (private and public member funcs)

--- CPP_Module_01/ex05/Harl.hpp
+++ CPP_Module_01/ex05/Harl.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
+// Headers
 #include <iostream>
 
 
+// Ansi colors
+#define RED		"\033[31m"
+#define GREEN		"\033[32m"
+#define YELLOW		"\033[33m"
+#define BLUE		"\033[34m"
+#define CYAN		"\033[36m"
+#define RESET		"\033[0m"
+#define BOLD		"\033[1m"
+
+
+// Harl class declaration
 class Harl {
 	private:
 		void	debug( void );
--- CPP_Module_01/ex05/main.cpp
+++ CPP_Module_01/ex05/main.cpp
@@ -13,9 +13,15 @@
  * 			TODO:...
 */
 
+#include "Harl.hpp"
 
 int main()
 {
+	Harl	harl;
 
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
 	return (0);
 }
