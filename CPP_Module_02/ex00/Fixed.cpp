ef706458f2c68e7f987942a784e17e7d8035dbea
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-09-13 11:02:12 +0100

    feat(CPP_02): ex00 - Implement Fixed class with Orthodox Canonical form and core functionality

--- CPP_Module_02/ex00/Fixed.cpp
+++ CPP_Module_02/ex00/Fixed.cpp
@@ -1,1 +1,42 @@
 #include "Fixed.hpp"
+
+// Default constructor
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+	fixedPointValue = 0;
+}
+
+// Copy constructor
+Fixed::Fixed( const Fixed &obj )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	fixedPointValue = obj.fixedPointValue;
+}
+
+// Copy assignment operator
+Fixed& Fixed::operator=( const Fixed &obj )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->fixedPointValue = obj.fixedPointValue;
+	return (*this);
+}
+
+// Destructor
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+
+// member functions
+
+// getter
+int	Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (fixedPointValue);
+}
+
+// setter
+void	Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fixedPointValue = raw;
+}
