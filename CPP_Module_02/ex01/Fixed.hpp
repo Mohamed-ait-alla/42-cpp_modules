b1fb50ca3821a4e6e89ad9a2811b319623287e4b
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-09-13 12:32:42 +0100

    feat(CPP_02): ex01 - Define Fixed class structure in header file

---
+++ CPP_Module_02/ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <ostream>
+#include <cmath>
+
+class Fixed {
+	private:
+		int					_fixedPointValue;
+		static const int	_nbFractionalBits = 8;
+	public:
+		Fixed();
+		Fixed( int const intValue);
+		Fixed( float const flaotValue);
+		Fixed(const Fixed &obj);
+		~Fixed();
+		Fixed& 	operator=(const Fixed &obj);
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+
+};
+
+std::ostream&	operator<<(std::ostream& cout, const Fixed& fixedObj);
