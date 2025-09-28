faca2e499ef49722a096a648322b56da67ab94a5
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-09-28 16:01:33 +0100

    feat(CPP_03/ex00): Implement ClapTrap class's member functions (attack(), takeDamage(), beRepaired())

--- CPP_Module_03/ex00/ClapTrap.cpp
+++ CPP_Module_03/ex00/ClapTrap.cpp
@@ -3,7 +3,7 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/09/27 10:30 by mait-all                        //
-//                               Updated: 2025/09/27 18:12 by mait-all                        //
+//                               Updated: 2025/09/28 12:36 by mait-all                        //
 // ****************************************************************************************** //
 
 #include "ClapTrap.hpp"
@@ -53,4 +53,50 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &other)
 ClapTrap::~ClapTrap()
 {
 	std::cout << "Destructor called" << std::endl;
+}
+
+// --- Member functions ---
+void	ClapTrap::attack(const std::string& target) {
+	if (_hitPoints <= 0)
+	{
+		std::cout << "ClapTrap: " << _name << " is dead!" << std::endl;
+		return ;
+	}
+	if (_energyPoints <= 0)
+	{
+		std::cout << "ClapTrap: " << _name << " can not attacks " << target << ": has no energy points!" << std::endl;
+		return ;
+	}
+	_energyPoints--;
+	std::cout << "ClapTrap: " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	if (_hitPoints <= 0)
+	{
+		std::cout << "ClapTrap: " << _name << " is already dead!" << std::endl;
+		return ;
+	}
+	// Prevent negative hit points
+	if (amount >= _hitPoints)
+		_hitPoints = 0;
+	else
+		_hitPoints -= amount;
+	std::cout << "ClapTrap: " << _name << " takes " << amount << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	if (_hitPoints <= 0)
+	{
+		std::cout << "ClapTrap: " << _name << " is dead!" << std::endl;
+		return ;
+	}
+	if (_energyPoints <= 0)
+	{
+		std::cout << "ClapTrap: " << _name << " can not be repaired: has no energy left!" << std::endl;
+		return ;
+	}
+	_hitPoints += amount;
+	_energyPoints--;
+	std::cout << "ClapTrap: " << _name << " is repaired for " << amount << " hit points!" << std::endl;
 }
