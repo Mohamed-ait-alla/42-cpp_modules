b27eabc3292a667c94cd66a48b7a4eee0a7d142f
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-09-28 17:24:07 +0100

    feat(CPP_03/ex00): add tests to main function

--- CPP_Module_03/ex00/main.cpp
+++ CPP_Module_03/ex00/main.cpp
@@ -2,6 +2,9 @@
  * @author:
  * 			ToDo...
  * 
+ * @github:
+ * 			ToDo...
+ * 
  * @description:
  * 				ToDo...
  * 
@@ -13,5 +16,21 @@
 
 int main( void )
 {
+	ClapTrap claptrap("Teo");
+	ClapTrap claptrap2(claptrap);
+	ClapTrap claptrap4;
+	claptrap4 = claptrap;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	claptrap.attack("enemy");
+	claptrap.takeDamage(9);
+	claptrap.attack("enemy");
+	// claptrap.takeDamage(2);
+	// claptrap.takeDamage(2);
+	claptrap.attack("enemy");
+	claptrap.beRepaired(10);
+	claptrap.attack("enemy");
 	return (0);
 }
