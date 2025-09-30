4d094ab5946bb188ce4f228c3a724d830b067949
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-09-30 10:43:00 +0100

    feat(CPP_03/ex02): add tests to main function

--- CPP_Module_03/ex02/main.cpp
+++ CPP_Module_03/ex02/main.cpp
@@ -16,12 +16,42 @@
 
 int main( void )
 {
-	FragTrap a("securityBot");
+	std::cout << "=== CPP03 EX02 FRAGTRAP TESTS ===" << std::endl;
+	std::cout << std::endl;
 
-	a.attack("enemy");
-	a.takeDamage(100);
-	a.attack("enemy");
-	a.highFivesGuys();
+	// Test 1: Basic Construction
+	std::cout << "--- Test 1: Constructor Tests ---" << std::endl;
+	FragTrap frag1("GuardBot");
+	FragTrap frag2;
+	FragTrap frag3(frag1);
+	FragTrap frag4;
+	frag4 = frag1; 
 
+	std::cout << std::endl;
+
+	// Inheritance: FragTrap can do everthing ClapTrap can
+	std::cout << "--- Test 2: Inherited Functionality ---" << std::endl;
+	frag1.attack("Intruder");
+	frag1.takeDamage(30);
+	frag1.beRepaired(15);
+
+	std::cout << std::endl;
+
+   std::cout << "--- Test 3: FragTrap Special Ability ---" << std::endl;
+	frag1.highFivesGuys();
+	frag2.highFivesGuys();
+
+	std::cout << std::endl;
+
+  std::cout << "--- Test 4: Hit Point Management ---" << std::endl;
+	FragTrap frag5("ToughBot");
+	frag5.takeDamage(99);  // Almost dead
+	frag5.attack("Enemy"); // Should work
+	frag5.takeDamage(5);   // Should die
+	frag5.attack("Enemy"); // Should fail - dead
+	frag5.beRepaired(10);  // Should fail - dead
+	frag5.highFivesGuys();
+
+	std::cout << std::endl;
 	return (0);
 }
