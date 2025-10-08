2bd78f0cba18210efc4c16a1af773da655ee858f
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-10-08 09:45:41 +0100

    feat(CPP_04/ex00): Implement the orthodox canonical form of Cat and Dog classes

--- CPP_Module_04/ex00/Cat.cpp
+++ CPP_Module_04/ex00/Cat.cpp
@@ -3,6 +3,30 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/08 09:08 by mait-all                        //
-//                               Updated: 2025/10/08 09:09 by mait-all                        //
+//                               Updated: 2025/10/08 09:38 by mait-all                        //
 // ****************************************************************************************** //
 
+#include "Cat.hpp"
+
+// Default constructor
+Cat::Cat() {
+	this->type = "Cat";
+	std::cout << "Cat: Default constructor called" << std::endl;
+}
+
+// Copy constructor
+Cat::Cat(const Cat& other) : Animal(other) {
+	std::cout << "Cat: Copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+Cat&	Cat::operator=(const Cat& other) {
+	Animal::operator=(other);
+	std::cout << "Cat: Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+// Destructor
+Cat::~Cat() {
+	std::cout << "Destructor" << std::endl;
+}
--- CPP_Module_04/ex00/Dog.cpp
+++ CPP_Module_04/ex00/Dog.cpp
@@ -3,6 +3,31 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/08 09:08 by mait-all                        //
-//                               Updated: 2025/10/08 09:09 by mait-all                        //
+//                               Updated: 2025/10/08 09:43 by mait-all                        //
 // ****************************************************************************************** //
 
+#include "Dog.hpp"
+
+
+// Default constructor
+Dog::Dog() {
+	this->type = "Dog";
+	std::cout << "Dog: Default constructor called" << std::endl;
+}
+
+// Copy constructor
+Dog::Dog(const Dog& other) : Animal(other) {
+	std::cout << "Dog: Copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+Dog&	Dog::operator=(const Dog& other) {
+	Animal::operator=(other);
+	std::cout << "Dog: Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+// Destructor
+Dog::~Dog() {
+	std::cout << "Dog: Destructor";
+}
