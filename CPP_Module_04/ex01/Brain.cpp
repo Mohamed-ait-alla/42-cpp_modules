13075eee4c6a2822689314303fdaa1082b4631d7
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-11 16:29:26 -0400

    feat(CPP_04/ex01): Implement the orthodox canonical form of Brain class

--- CPP_Module_04/ex01/Brain.cpp
+++ CPP_Module_04/ex01/Brain.cpp
@@ -3,5 +3,32 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/11 20:15 by mait-all                        //
-//                               Updated: 2025/10/11 20:15 by mait-all                        //
+//                               Updated: 2025/10/11 21:27 by mait-all                        //
 // ****************************************************************************************** //
+
+#include "Brain.hpp"
+
+// Default constructor
+Brain::Brain() {
+    std::cout << "Brain: Default constructor called" << std::endl;
+}
+
+// Copy constructor
+Brain::Brain(const Brain& other) {
+    for (int i = 0; i < 100; i++)
+        this->ideas[i] = other.ideas[i];
+    std::cout << "Brain: Copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+Brain&  Brain::operator=(const Brain& other) {
+    for (int i = 0; i < 100; i++)
+        this->ideas[i] = other.ideas[i];
+    std::cout << "Brain: Copy assignment operator called" << std::endl;
+    return (*this);
+}
+
+// Destructor
+Brain::~Brain() {
+    std::cout << "Brain: Destructor called" << std::endl;
+}
