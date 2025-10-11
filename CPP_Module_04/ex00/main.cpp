299c8507cda8c77439fccf5329eb4d2d608baf2f
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-11 10:21:23 -0400

    feat(CPP_04/ex00): add tests to main function

--- CPP_Module_04/ex00/main.cpp
+++ CPP_Module_04/ex00/main.cpp
@@ -20,24 +20,41 @@
 
 int main(void)
 {
-	const WrongAnimal* meta = new WrongAnimal();
-	// const WrongAnimal* i = new Dog();
-	const WrongAnimal* j = new WrongCat();
+	const Animal* meta = new Animal();
+	const Animal* i = new Dog();
+	const Animal* j = new Cat();
 	
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	// std::cout << "Type of this Animal is: " << i->getType() << " " << std::endl;
+	std::cout << "Type of this Animal is: " << i->getType() << " " << std::endl;
 	std::cout << "Type of this Animal is: " << j->getType() << " " << std::endl;
-	// i->makeSound();
+	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
 
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	delete meta;
-	// delete i;
-	delete j;
+	delete meta; delete i; delete j;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	// === lack of polymorphism tests ===
+	const	WrongAnimal* wrongMeta = new WrongAnimal();
+	const	WrongAnimal* wrongA = new WrongCat();
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Type of this Wrong Animal is: " << wrongA->getType() << " " << std::endl;
+	wrongA->makeSound();
+	wrongMeta->makeSound();
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	 
+	delete wrongMeta; delete wrongA;
 	return (0);
 }
