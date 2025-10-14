a4fc934039a654050a14af75a68f9028c3c8c791
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-14 14:38:42 -0400

    feat(CPP_04/ex03): implement a derived Cure class

--- CPP_Module_04/ex03/Cure.cpp
+++ CPP_Module_04/ex03/Cure.cpp
@@ -3,8 +3,40 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/14 17:39 by mait-all                        //
-//                               Updated: 2025/10/14 17:39 by mait-all                        //
+//                               Updated: 2025/10/14 19:37 by mait-all                        //
 // ****************************************************************************************** //
 
 #include "Cure.hpp"
 
+// Default constructor
+Cure::Cure() {
+    this->type = "cure";
+    std::cout << "Cure: Default constructor called" << std::endl;
+}
+
+// Copy constructor
+Cure::Cure(const Cure& other) : AMateria(other) {
+    std::cout << "Cure: Copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+Cure&   Cure::operator=(const Cure& other) {
+    AMateria::operator=(other);
+    std::cout << "Cure: Copy assignment operator called" << std::endl;
+    return (*this);
+}
+
+// Destructor
+Cure::~Cure() {
+    std::cout << "Cure: Destructor called" << std::endl;
+}
+
+
+// --- Member functions ---
+AMateria*   Cure::clone(void) const {
+    return (new Cure());
+}
+
+void    Cure::use(ICharacter& target) {
+    std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+}
--- CPP_Module_04/ex03/Cure.hpp
+++ CPP_Module_04/ex03/Cure.hpp
@@ -3,5 +3,29 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/14 17:39 by mait-all                        //
-//                               Updated: 2025/10/14 17:39 by mait-all                        //
+//                               Updated: 2025/10/14 18:47 by mait-all                        //
 // ****************************************************************************************** //
+
+#pragma once
+
+#include "AMateria.hpp"
+
+
+// ************************************************************************** //
+//                                Cure Class                                  //
+// ************************************************************************** //
+
+class Cure : public AMateria {
+
+    public:
+        // ocf
+        Cure();
+        Cure(const Cure& other);
+        Cure&   operator=(const Cure& other);
+        ~Cure();
+
+        // member functions
+        AMateria*   clone(void) const;
+        void        use(ICharacter& target);
+
+};
