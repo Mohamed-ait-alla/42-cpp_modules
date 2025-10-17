8355e2b5166eff2f3ce0f03ad589fd0d49e5620b
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-17 06:11:06 -0400

    feat(CPP_04/ex03): define MateriaSource class in header file, which will implement the IMateriaSource interface

---
+++ CPP_Module_04/ex03/MateriaSource.hpp
@@ -0,0 +1,34 @@
+// ****************************************************************************************** //
+//                               file: MateriaSouce.hpp                                       //
+//                               by: mait-all <mait-all@student.1337.ma>                      //
+//                                                                                            //
+//                               Created: 2025/10/17 10:57 by mait-all                        //
+//                               Updated: 2025/10/17 11:06 by mait-all                        //
+// ****************************************************************************************** //
+
+#pragma once
+
+#include "IMateriaSource.hpp"
+
+
+// ************************************************************************** //
+//                              MateriaSource Class                           //
+// ************************************************************************** //
+
+class MateriaSource : public IMateriaSource {
+    
+    public:
+        // ocf
+        MateriaSource();
+        MateriaSource(const MateriaSource& other);
+        MateriaSource&  operator=(const MateriaSource& other);
+        ~MateriaSource();
+
+        // member functions
+        void        learnMateria(AMateria* m);
+        AMateria*   createMateria(std::string const & type);
+
+    private:
+        AMateria*   materias[4];
+
+};
