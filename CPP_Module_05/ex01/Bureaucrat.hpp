d9593f6df043367c73b190612634dd74b0b30e6d
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-20 06:34:21 -0400

    feat(CPP_05/ex01): initialize exercice with main files

---
+++ CPP_Module_05/ex01/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+// ****************************************************************************************** //
+//                               file: Bureaucrat.hpp                                         //
+//                               by: mait-all <mait-all@student.1337.ma>                      //
+//                                                                                            //
+//                               Created: 2025/10/20 11:29 by mait-all                        //
+//                               Updated: 2025/10/20 11:29 by mait-all                        //
+// ****************************************************************************************** //
+
+#pragma once
+
+#include <iostream>
+
+
+// ************************************************************************** //
+//                               Bureaucrat Class                             //
+// ************************************************************************** //
+
+class Bureaucrat {
+
+    public:
+        // ocf
+        Bureaucrat();
+        Bureaucrat(const std::string name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+
+        // member functions
+        std::string   getName() const;
+        int           getGrade() const;
+        void          incrementGrade();
+        void          decrementGrade();
+
+        // Exceptions
+        class GradeTooHighException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+    private:
+        const std::string   _name;
+        int                 _grade;
+
+};
+
+// insertion operator (<<) overloading
+std::ostream&	operator<<(std::ostream& output, const Bureaucrat& Obj);
---
+++ CPP_Module_05/ex01/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * @author:
+ * 			ToDo...
+ * 
+ * @github:
+ * 			ToDo...
+ * 
+ * @description:
+ * 			ToDo...
+ * 
+ * 
+ * @usage:
+ * 			ToDo...
+ *
+*/
+
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+
+    return 0;
+}
