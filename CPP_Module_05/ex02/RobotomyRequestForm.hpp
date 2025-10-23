3c429f8e01c31ee6197b4221c1f0f68640433791
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-23 05:09:57 -0400

    feat(CPP_05/ex02): declare RobotomyRequestForm class in header file

--- CPP_Module_05/ex02/RobotomyRequestForm.hpp
+++ CPP_Module_05/ex02/RobotomyRequestForm.hpp
@@ -3,5 +3,32 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/22 13:50 by mait-all                        //
-//                               Updated: 2025/10/22 13:50 by mait-all                        //
+//                               Updated: 2025/10/23 10:07 by mait-all                        //
 // ****************************************************************************************** //
+
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+# include "AForm.hpp"
+
+
+// ************************************************************************** //
+//                           RobotomyRequestForm Class                        //
+// ************************************************************************** //
+
+class RobotomyRequestForm : public AForm {
+
+    public:
+        // ocf
+        RobotomyRequestForm();
+        RobotomyRequestForm(const std::string target);
+        RobotomyRequestForm(const RobotomyRequestForm& other);
+        RobotomyRequestForm&    operator=(const RobotomyRequestForm& other);
+        ~RobotomyRequestForm();
+
+        // member functions
+        void    execute(const Bureaucrat& executor);
+
+};
+
+
+#endif /* __ROBOTOMYREQUESTFORM_HPP__ */
