4c5c8dabeb0b9764acd8a82d81a9426f769185a1
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-23 05:58:21 -0400

    feat(CPP_05/ex02): declare PresidentialPardonForm class in header file

--- CPP_Module_05/ex02/PresidentialPardonForm.hpp
+++ CPP_Module_05/ex02/PresidentialPardonForm.hpp
@@ -3,5 +3,32 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/22 13:50 by mait-all                        //
-//                               Updated: 2025/10/22 13:50 by mait-all                        //
+//                               Updated: 2025/10/23 10:55 by mait-all                        //
 // ****************************************************************************************** //
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+# include "AForm.hpp"
+
+
+// ************************************************************************** //
+//                         PresidentialPardonForm Class                       //
+// ************************************************************************** //
+
+class PresidentialPardonForm : public AForm {
+
+    public:
+        // ocf
+        PresidentialPardonForm();
+        PresidentialPardonForm(const std::string target);
+        PresidentialPardonForm(const PresidentialPardonForm& other);
+        PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
+        ~PresidentialPardonForm();
+
+        // member functions
+        void    execute(const Bureaucrat& executor);
+
+};
+
+
+#endif /* __PRESIDENTIALPARDONFORM_HPP__ */
