4ba6c4a9d8c256f68cb835067929cfa7511d69ae
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-24 16:05:30 -0400

    feat(CPP_05/ex03): implement Intern class functionalities (ocf, member funcs)

--- CPP_Module_05/ex03/Intern.cpp
+++ CPP_Module_05/ex03/Intern.cpp
@@ -3,5 +3,60 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/23 19:38 by mait-all                        //
-//                               Updated: 2025/10/23 19:38 by mait-all                        //
+//                               Updated: 2025/10/24 21:00 by mait-all                        //
 // ****************************************************************************************** //
+
+#include "Intern.hpp"
+
+// -------------------------------
+// Orthodox Canonical Form
+// -------------------------------
+
+// Default constructor
+Intern::Intern() {}
+
+// Copy constructor
+Intern::Intern(const Intern& other)
+{
+    *this = other;
+}
+
+// Copy assignment operator
+Intern& Intern::operator=(const Intern& other) {
+    if (this != &other)
+        *this = other;
+    return (*this);
+}
+
+// Destructor
+Intern::~Intern() {}
+
+
+// -------------------------------
+// Member functions
+// -------------------------------
+
+AForm*  Intern::makeForm(const std::string formName, const std::string formTarget) {
+    std::string validForms[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+    int         index;
+
+    for (index = 0; index < 3; index++)
+    {
+        if (formName == validForms[index])
+            break;
+        index++;
+    }
+
+    switch(index)
+    {
+        case 0:
+                return (new ShrubberyCreationForm(formTarget));
+        case 1:
+                return (new RobotomyRequestForm(formTarget));
+        case 2:
+                return (new PresidentialPardonForm(formTarget));
+        default:
+                std::cout << "Oops! Invalid form name" << std::endl;
+                return (NULL);
+    }
+}
