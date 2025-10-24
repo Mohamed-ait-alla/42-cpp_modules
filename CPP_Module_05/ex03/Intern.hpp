e8f6390fda7f49ff2c7ee093c7f5c5deac7f3a8f
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-24 12:54:19 -0400

    feat(CPP_05/ex03): declare Intern class structure in header file

--- CPP_Module_05/ex03/Intern.hpp
+++ CPP_Module_05/ex03/Intern.hpp
@@ -3,5 +3,33 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/23 19:38 by mait-all                        //
-//                               Updated: 2025/10/23 19:38 by mait-all                        //
+//                               Updated: 2025/10/24 17:52 by mait-all                        //
 // ****************************************************************************************** //
+
+#ifndef INTERN_HPP
+# define INTERN_HPP
+# include "ShrubberyCreationForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "PresidentialPardonForm.hpp"
+
+
+// ************************************************************************** //
+//                                Intern Class                                //
+// ************************************************************************** //
+
+class Intern {
+
+    public:
+        // ocf
+        Intern();
+        Intern(const Intern& other);
+        Intern& operator=(const Intern& other);
+        ~Intern();
+
+        // member functions
+        AForm*  makeForm(const std::string formName, const std::string formTarget);
+
+};
+
+
+#endif /* __INTERN_HPP__ */
