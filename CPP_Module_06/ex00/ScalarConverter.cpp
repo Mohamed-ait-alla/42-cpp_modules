25baba7f8e61fe01bb4748cd6ac15b0e7719c3c0
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-26 15:25:20 -0400

    feat(CPP_06/ex00): implement the orthodox canonical form of ScalarConverter class

--- CPP_Module_06/ex00/ScalarConverter.cpp
+++ CPP_Module_06/ex00/ScalarConverter.cpp
@@ -3,5 +3,34 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/26 19:49 by mait-all                        //
-//                               Updated: 2025/10/26 19:49 by mait-all                        //
+//                               Updated: 2025/10/26 20:23 by mait-all                        //
 // ****************************************************************************************** //
+
+#include "ScalarConverter.hpp"
+
+// -------------------------------
+// Orthodox Canonical Form
+// -------------------------------
+
+// Default constructor
+ScalarConverter::ScalarConverter() {}
+
+// Copy constructor
+ScalarConverter::ScalarConverter(const ScalarConverter& other) {
+    *this = other;
+}
+
+// Copy assignment operator
+ScalarConverter&    ScalarConverter::operator=(const ScalarConverter& other) {
+    if (this != &other)
+        *this = other;
+    return (*this);
+}
+
+// Destructor
+ScalarConverter::~ScalarConverter() {}
+
+
+// -------------------------------
+// Member functions
+// -------------------------------
