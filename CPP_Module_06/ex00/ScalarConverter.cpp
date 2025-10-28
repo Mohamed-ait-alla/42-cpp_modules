6b34a4f6d636c6e30321382f83b7fe81e78d022c
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-28 17:18:49 -0400

    fix(CPP_06/ex00): fix some displaying issues in float or double numbers

--- CPP_Module_06/ex00/ScalarConverter.cpp
+++ CPP_Module_06/ex00/ScalarConverter.cpp
@@ -3,7 +3,7 @@
 //                               by: mait-all <mait-all@student.1337.ma>                      //
 //                                                                                            //
 //                               Created: 2025/10/26 19:49 by mait-all                        //
-//                               Updated: 2025/10/28 13:19 by mait-all                        //
+//                               Updated: 2025/10/28 21:25 by mait-all                        //
 // ****************************************************************************************** //
 
 #include "ScalarConverter.hpp"
@@ -63,47 +63,75 @@ void    ScalarConverter::convert(std::string& input) {
     {
     case CHAR:
         {
+            // scalar type: char
             if (isprint(input[0]))
                 std::cout << "char: '" << input << "'" << std::endl;
             else
                 std::cout << "char: Non Displayable" << std::endl;
+            // scalar type: int
             std::cout << "int: " << static_cast<int>(input[0]) << std::endl;
+            // scalar type: float
             std::cout << "float: " << static_cast<float>(input[0]) << ".0f" << std::endl;
+            // scalar type: double
             std::cout << "double: " << static_cast<double>(input[0]) << ".0" << std::endl;
             break;
         }
     case INT:
         {
+            // scalar type: char
             char    castedChar = static_cast<char>(atoi(input.c_str()));
             if (isprint(castedChar))
                 std::cout << "char: '" << castedChar << "'" << std::endl;
             else
                 std::cout << "char: Non Displayable" << std::endl;
+            // scalar type: int
             std::cout << "int: " << atoi(input.c_str()) << std::endl;
+            // scalar type: float
             std::cout << "float: " << static_cast<float>(atoi(input.c_str())) << ".0f" << std::endl;
+            // scalar type: double
             std::cout << "double: " << static_cast<double>(atoi(input.c_str())) << ".0" << std::endl;
             break;
         }
-    case FLOAT: // note: edge case 42.0f 42.2f
+    case FLOAT:
         {
+            // scalar type: char
             if (isprint(static_cast<char>(atoi(input.c_str()))))
                 std::cout << "char: '" << static_cast<char>(atoi(input.c_str())) << "'" << std::endl;
             else
                 std::cout << "char: Non Displayable" << std::endl;
+            // scalar type: int
             std::cout << "int: " << static_cast<int>(atof(input.c_str())) << std::endl;
-            std::cout << "float: " << atof(input.c_str()) << ".0f" << std::endl;
-            std::cout << "double: " << static_cast<double>(strtod(input.c_str(), 0)) << ".0" << std::endl;
+            // scalar type: float
+            std::cout << "float: " << atof(input.c_str());
+            if (atof(input.c_str()) == static_cast<int>(atof(input.c_str())))
+                std::cout << ".0";
+            std::cout << "f" << std::endl;
+            // scalar type: double
+            std::cout << "double: " << static_cast<double>(strtod(input.c_str(), 0));
+            if (atof(input.c_str()) == static_cast<int>(strtod(input.c_str(), 0)))
+                std::cout << ".0";
+            std::cout << std::endl;
             break;
         }
-    case DOUBLE: // note: edge case 42.0 42.1
+    case DOUBLE:
         {
+            // scalar type: char
             if (isprint(static_cast<char>(atof(input.c_str()))))
                 std::cout << "char: '" << static_cast<char>(atof(input.c_str())) << "'" << std::endl;
             else
                 std::cout << "char: Non Displayable" << std::endl;
+            // scalar type: int
             std::cout << "int: " << static_cast<int>(atof(input.c_str())) << std::endl;
-            std::cout << "float: " << atof(input.c_str()) << ".0f" << std::endl;
-            std::cout << "double: " << static_cast<double>(strtod(input.c_str(), 0)) << std::endl;
+            // scalar type: float
+            std::cout << "float: " << atof(input.c_str());
+            if (atof(input.c_str()) == static_cast<int>(atof(input.c_str())))
+                std::cout << ".0";
+            std::cout << "f" << std::endl;
+            // scalar type: double
+            std::cout << "double: " << static_cast<double>(strtod(input.c_str(), 0));
+            if (atof(input.c_str()) == static_cast<int>(strtod(input.c_str(), 0)))
+                std::cout << ".0";
+            std::cout << std::endl;
             break;
         }
     
