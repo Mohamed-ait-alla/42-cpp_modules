1f37b33d1bc5191c69a696df13f35a5b471213aa
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-19 19:39:44 +0100

    feat(CPP_06/ex01): define the Serializer class structure in header file

--- CPP_Module_06/ex01/Serializer.hpp
+++ CPP_Module_06/ex01/Serializer.hpp
@@ -3,6 +3,39 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/11/19 14:53 by mait-all                   //
-//                        Updated: 2025/11/19 14:54 by mait-all                   //
+//                        Updated: 2025/11/19 19:37 by mait-all                   //
 // ****************************************************************************** //
 
+#ifndef	SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <iostream>
+#include <cstdint>
+
+// Data 
+typedef struct t_Data {
+	int	x;
+} Data;
+
+// ****************************************************************************** //
+//                               Serializer Class                                 //
+// ****************************************************************************** //
+
+class Serializer {
+
+	public:
+		// member functions
+		static uintptr_t	serialize(Data* ptr);
+		static Data*		deserialize(uintptr_t raw);
+
+	private:
+		// ocf
+		// ocf is private to make the class instantiable
+		Serializer();
+		Serializer(const Serializer& other);
+		Serializer&	operator=(const Serializer& other);
+		~Serializer();
+};
+
+
+#endif /* __SERIALIZER_HPP__ */
