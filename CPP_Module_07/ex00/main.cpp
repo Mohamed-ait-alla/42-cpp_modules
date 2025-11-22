71a2a86cc948e854eb49f58f621126ef2432e1cd
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-22 20:47:42 +0100

    feat(CPP_07/ex00): add subject test to main function

--- CPP_Module_07/ex00/main.cpp
+++ CPP_Module_07/ex00/main.cpp
@@ -19,29 +19,21 @@
 
 int	main(void)
 {
+	int a = 2;
+	int b = 3;
 
-	std::cout << "----------------------- swap template test -----------------------" << std::endl;
-	std::cout << std::endl;
-	{
-		std::string a = "hello";
-		std::string b = "world";
-		std::cout << "before => a: " << a << " b: " << b << std::endl;
-		::swap<std::string>(a, b);
-		std::cout << "after => a: " << a << " b: " << b << std::endl;
-	}
-	std::cout << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::cout << "----------------------- min max templates test -----------------------" << std::endl;
-	std::cout << std::endl;
-	{
-		int a = 6;
-		int b = 4;
-	
-		std::cout << "min is: " << ::min<int>(a, b) << std::endl;
-		std::cout << "max is: " << ::max<int>(a, b) << std::endl;
-	}
-	std::cout << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
-	return (0);
+	return 0;
 }
