71d3302a4ba9bf4a8b1c30f9bd1f175ae93bed67
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-22 20:42:48 +0100

    feat(CPP_07/ex00): implement the min, max template functions

--- CPP_Module_07/ex00/main.cpp
+++ CPP_Module_07/ex00/main.cpp
@@ -20,10 +20,28 @@
 int	main(void)
 {
 
-	std::string a = "hello";
-	std::string b = "world";
-	std::cout << "before => a: " << a << " b: " << b << std::endl;
-	::swap<std::string>(a, b);
-	std::cout << "after => a: " << a << " b: " << b << std::endl;
+	std::cout << "----------------------- swap template test -----------------------" << std::endl;
+	std::cout << std::endl;
+	{
+		std::string a = "hello";
+		std::string b = "world";
+		std::cout << "before => a: " << a << " b: " << b << std::endl;
+		::swap<std::string>(a, b);
+		std::cout << "after => a: " << a << " b: " << b << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "----------------------- min max templates test -----------------------" << std::endl;
+	std::cout << std::endl;
+	{
+		int a = 6;
+		int b = 4;
+	
+		std::cout << "min is: " << ::min<int>(a, b) << std::endl;
+		std::cout << "max is: " << ::max<int>(a, b) << std::endl;
+	}
+	std::cout << std::endl;
+
+
 	return (0);
 }
--- CPP_Module_07/ex00/whatever.hpp
+++ CPP_Module_07/ex00/whatever.hpp
@@ -3,7 +3,7 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/11/22 17:51 by mait-all                   //
-//                        Updated: 2025/11/22 18:20 by mait-all                   //
+//                        Updated: 2025/11/22 20:41 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef WHATEVER_HPP
@@ -12,6 +12,9 @@
 #include <iostream>
 
 
+// === swap<T>() ============================================================== //
+// Swaps the values of two variables of type T.
+// ============================================================================ //
 template <typename T>
 void	swap(T& a, T& b)
 {
@@ -19,5 +22,24 @@ void	swap(T& a, T& b)
 	a = b;
 	b = tmp;
 }
+
+// === min<T>() =============================================================== //
+// Returns the smaller of two values of type T.
+// ============================================================================ //
+template <typename T>
+T		min(T a, T b)
+{
+	return (a < b ? a : b);
+}
+
+// === max<T>() =============================================================== //
+// Returns the larger of two values of type T.
+// ============================================================================ //
+template <typename T>
+T		max(T a, T b)
+{
+	return (a > b ? a : b);
+}
+
 
 #endif /* __WHATEVER_HPP__ */
