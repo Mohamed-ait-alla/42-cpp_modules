1b0cf194a3f3cccbb969f07a979e46fbfea6eaec
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-23 09:07:53 +0100

    feat(CPP_07/ex01): initialize exercise with main files

---
+++ CPP_Module_07/ex01/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * @author:
+ * 			mait-all
+ * 
+ * @github:
+ * 			https://github.com/Mohamed-ait-alla
+ * 
+ * @description:
+ * 			ToDo...
+ * 
+ * 
+ * @usage:
+ * 			1. Compile:	make
+ * 			2. Run:		./iter
+ * 			3. CleanUp:	make fclean
+*/
+
+#include "iter.hpp"
+
+int	main(void)
+{
+
+	return (0);
+}
