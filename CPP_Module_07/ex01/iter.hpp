f3246dda18ffa17e533c8aad652a2aaa208cee2f
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-23 19:54:06 +0100

    feat(CPP_07/ex01): implement the iter template function

--- CPP_Module_07/ex01/iter.hpp
+++ CPP_Module_07/ex01/iter.hpp
@@ -3,11 +3,26 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/11/23 09:02 by mait-all                   //
-//                        Updated: 2025/11/23 09:04 by mait-all                   //
+//                        Updated: 2025/11/23 19:50 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef ITER_HPP
 # define ITER_HPP
+
+# include <iostream>
+
+
+// === iter<T, Func>() ======================================================== //
+// calling the function (func) on each element of the passed array
+// ============================================================================ //
+template <typename T, typename Func>
+void	iter(T* arr, const size_t len, Func func)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		func(arr[i]);
+	}
+}
 
 
 #endif /* __ITER_HPP__ */
