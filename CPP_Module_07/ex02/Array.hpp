af58cb017c1f37a1132be464860bb119d97572c4
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-24 11:01:18 +0100

    feat(CPP_07/ex02): define the full structure of Array template class in header file

--- CPP_Module_07/ex02/Array.hpp
+++ CPP_Module_07/ex02/Array.hpp
@@ -1,13 +1,41 @@
 // ****************************************************************************** //
-//                        file: Array.hpp                                    //
+//                        file: Array.hpp                                         //
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/11/24 09:54 by mait-all                   //
-//                        Updated: 2025/11/24 09:54 by mait-all                   //
+//                        Updated: 2025/11/24 10:59 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
+
+
+// ****************************************************************************** //
+//                             Array Class Template                               //
+// ****************************************************************************** //
+
+template <typename T> class Array {
+
+	public:
+		// ocf
+		Array();
+		Array(unsigned int n);
+		Array(const Array& other);
+		Array&	operator=(const Array& other);
+		~Array();
+
+		// operators
+		T&				operator[](size_t index);
+		const T&		operator[](size_t index) const;	
+
+		// member functions
+		unsigned int	size() const;
+
+	private:
+		T*				_arr;
+		unsigned int	_len;
+
+};
 
 
 #endif /* __ARRAY_HPP__ */
