246f90783729990df8fbe4fea8293d9988e79c0f
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-27 09:32:39 +0100

    feat(CPP_08/ex00): initialize exercise with main files

---
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -0,0 +1,13 @@
+// ****************************************************************************** //
+//                        file: easyfind.hpp                                      //
+//                        by: mait-all <mait-all@student.1337.ma>                 //
+//                                                                                //
+//                        Created: 2025/11/27 09:29 by mait-all                   //
+//                        Updated: 2025/11/27 09:30 by mait-all                   //
+// ****************************************************************************** //
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+
+#endif /* __EASYFIND_HPP__ */
