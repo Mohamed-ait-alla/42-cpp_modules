387fd390d186fcbb3a0e87fe050c4302572d7360
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-27 10:48:12 +0100

    feat(CPP_08/ex00): implement the easyfind() template function

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -3,11 +3,28 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/11/27 09:29 by mait-all                   //
-//                        Updated: 2025/11/27 09:30 by mait-all                   //
+//                        Updated: 2025/11/27 10:44 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
+
+# include <iostream>
+# include <vector>
+# include <algorithm>
+
+
+// === easyfind<T>() ========================================================== //
+// finds an element within a container
+// ============================================================================ //
+template <typename T>
+int	easyfind(T& container , int toFind)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), toFind);
+	if (it != container.end())
+		return (*it);
+	throw std::runtime_error("Element not found in container");
+}
 
 
 #endif /* __EASYFIND_HPP__ */
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -18,6 +18,22 @@
 
 int	main(void)
 {
+	try
+	{
+		std::vector<int> v;
+		v.push_back(3);
+		v.push_back(4);
+		v.push_back(2);
+		v.push_back(7);
+		int	found = easyfind(v, 0);
+		std::cout << "Element " << found << " found in container" << std::endl;  
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
 
 	return (0);
 }
