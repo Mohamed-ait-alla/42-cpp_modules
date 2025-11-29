9e0baa91b39a2864b42984565c10a41ea3a2cd12
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-29 12:03:54 +0100

    feat(CPP_08/ex02): initialize exercise with main files

---
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -0,0 +1,13 @@
+// ****************************************************************************** //
+//                        file: MutantStack.hpp                                   //
+//                        by: mait-all <mait-all@student.1337.ma>                 //
+//                                                                                //
+//                        Created: 2025/11/29 12:00 by mait-all                   //
+//                        Updated: 2025/11/29 12:00 by mait-all                   //
+// ****************************************************************************** //
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+
+#endif /* __MUTANTSTACK_HPP__ */
