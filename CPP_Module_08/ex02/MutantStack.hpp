8677ddcb9f105d68f3d1dc6563c04a64c7141d94
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-11-30 09:17:00 +0100

    feat(CPP_08/ex02): implement the MutantStack class's functionality

--- CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -3,11 +3,37 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/11/29 12:00 by mait-all                   //
-//                        Updated: 2025/11/29 12:00 by mait-all                   //
+//                        Updated: 2025/11/30 09:09 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <iterator>
+
+// ****************************************************************************** //
+//                            MutantStack Class Template                          //
+// ****************************************************************************** //
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+
+	public:
+		typedef typename Container::iterator	iterator;
+
+		iterator	begin()
+		{
+			return (this->c.begin());
+		}
+
+		iterator	end()
+		{
+			return (this->c.end());
+		}
+
+};
 
 
 #endif /* __MUTANTSTACK_HPP__ */
--- CPP_Module_08/ex02/main.cpp
+++ CPP_Module_08/ex02/main.cpp
@@ -18,6 +18,20 @@
 
 int	main(void)
 {
+	MutantStack<int> mstack;
+
+	mstack.push(3);
+	mstack.push(4);
+	mstack.push(2);
+	mstack.push(1);
+
+	MutantStack<int>::iterator it;
+	MutantStack<int>::iterator ite = mstack.end();
+
+	for (it = mstack.begin(); it != ite; it++)
+	{
+		std::cout << *it << " ";
+	}
 
 	return (0);
 }
