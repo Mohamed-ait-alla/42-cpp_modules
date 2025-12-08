0f93147d76e9c8ec84b07a1a7742e54004b0ea2c
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-12-08 09:22:03 +0100

    feat(CPP_09/ex00): initialize exercise with main files

---
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,13 @@
+// ****************************************************************************** //
+//                        file: BitcoinExchange.hpp                               //
+//                        by: mait-all <mait-all@student.1337.ma>                 //
+//                                                                                //
+//                        Created: 2025/12/08 09:15 by mait-all                   //
+//                        Updated: 2025/12/08 09:15 by mait-all                   //
+// ****************************************************************************** //
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+
+#endif /* __BITCOINEXCHANGE_HPP__ */
---
+++ CPP_Module_09/ex00/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * @author:
+ * 			mait-all
+ *
+ * @github:
+ * 			https://github.com/Mohamed-ait-alla
+ *
+ * @description:
+ *          ToDo...
+ *
+ * @usage:
+ * 			1. Compile:	make
+ * 			2. Run:		./easyFind
+ * 			3. CleanUp:	make fclean
+*/
+
+#include "BitcoinExchange.hpp"
+
+int main(void)
+{
+
+    return (0);
+}
