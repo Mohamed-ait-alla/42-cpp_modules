d78c3397b1dd6305b414722abe33ff62c8e2cbfd
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-12-13 20:53:02 +0100

    feat(CPP_09/ex01): initialize exercise with main files

---
+++ CPP_Module_09/ex01/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * @author:
+ * 			mait-all
+ *
+ * @github:
+ * 			https://github.com/Mohamed-ait-alla
+ *
+ * @description:
+ *          ToDo...
+ *
+ * @usage:
+ * 			1. Compile:	make
+ * 			2. Run:		./easyFind
+ * 			3. CleanUp:	make fclean
+*/
+
+int	main(void)
+{
+
+	return (0);
+}
