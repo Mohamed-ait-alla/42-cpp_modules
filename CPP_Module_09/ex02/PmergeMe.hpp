943d904cf0e6a0c4f2ea1584d4f7243b8fe53376
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-12-15 14:30:07 +0100

    feat(CPP_09/ex02): initialize exercise with main files

---
+++ CPP_Module_09/ex02/PmergeMe.hpp
@@ -0,0 +1,13 @@
+// ****************************************************************************** //
+//                        file: PmergeMe.hpp                                      //
+//                        by: mait-all <mait-all@student.1337.ma>                 //
+//                                                                                //
+//                        Created: 2025/12/15 14:25 by mait-all                   //
+//                        Updated: 2025/12/15 14:25 by mait-all                   //
+// ****************************************************************************** //
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+
+#endif /* __PMERGEME_HPP__ */
---
+++ CPP_Module_09/ex02/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * @author:
+ * 			mait-all
+ *
+ * @github:
+ * 			https://github.com/Mohamed-ait-alla
+ *
+ * @description:
+ *          ToDo...
+ *
+ * @usage:
+ * 			1. Compile:	make
+ * 			2. Run:		./easyFind
+ * 			3. CleanUp:	make fclean
+*/
+
+#include "PmergeMe.hpp"
+
+int	main(int ac, char** av)
+{
+
+	return (0);
+}
