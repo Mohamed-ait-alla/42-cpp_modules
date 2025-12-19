57855100fe661b394bc449f571a4bde9160a5ba0
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-12-19 10:31:30 +0100

    feat(CPP_09/ex02): implement the first step in merge-insertion algorithm

--- CPP_Module_09/ex02/PmergeMe.cpp
+++ CPP_Module_09/ex02/PmergeMe.cpp
@@ -3,7 +3,7 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/12/15 14:25 by mait-all                   //
-//                        Updated: 2025/12/16 14:45 by mait-all                   //
+//                        Updated: 2025/12/19 10:30 by mait-all                   //
 // ****************************************************************************** //
 
 #include "PmergeMe.hpp"
@@ -27,6 +27,62 @@ bool	isValidArg(std::string arg)
 	return (true);
 }
 
+
+bool	compare(size_t a, size_t b)
+{
+	return (a < b);
+}
+
+void	swapPair(std::vector<int>& _vect, int leftPair, int rightPair, int pairLevel)
+{
+	for (int i = 0; i < pairLevel; i++)
+	{
+		std::swap(_vect[leftPair - i], _vect[rightPair - i]);
+	}
+}
+
+
+/**
+ * todo...
+ * 
+ * 
+ * @param input todo...
+*/
+void	mergeInsertionSort(std::vector<int>& input, int pairLevel)
+{
+	int totalPairs = input.size() / pairLevel;
+
+	if (totalPairs < 2)
+		return ;
+
+	// step 1: devide into pairs && sorting those pairs
+	bool hasOdd = (totalPairs % 2 != 0);
+
+	int	pairsToCompare = hasOdd ? totalPairs - 1 : totalPairs;
+
+	for (int i = 0; i < pairsToCompare ; i += 2)
+	{
+		// get the last indexes in each pair 
+		int	leftPairLast = (i + 1) * pairLevel - 1;
+		int	rightPairLast = (i + 2) * pairLevel - 1;
+
+		// compare and swap
+		if (compare(input[rightPairLast], input[leftPairLast]))
+			swapPair(input, leftPairLast, rightPairLast, pairLevel);
+	}
+
+	mergeInsertionSort(input, pairLevel * 2);
+
+	// step 2: insertion
+
+	
+
+
+
+
+}
+
+
 /**
  * todo...
  * 
@@ -49,18 +105,19 @@ void	processInput(int ac, char **av)
 		_deq.push_back(atoi(av[i]));
 	}
 
+	mergeInsertionSort(_vect, 1);
 	for (size_t i = 0; i < _vect.size(); i++)
 	{
 		std::cout << _vect[i] << " ";
 	}
 
-	std::cout << std::endl;
-	std::cout << std::endl;
+	// std::cout << std::endl;
+	// std::cout << std::endl;
 	
-	for (size_t i = 0; i < _deq.size(); i++)
-	{
-		std::cout << _deq[i] << " ";
-	}
-	std::cout << std::endl;
+	// for (size_t i = 0; i < _deq.size(); i++)
+	// {
+	// 	std::cout << _deq[i] << " ";
+	// }
+	// std::cout << std::endl;
 	
 }
--- CPP_Module_09/ex02/PmergeMe.hpp
+++ CPP_Module_09/ex02/PmergeMe.hpp
@@ -3,7 +3,7 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/12/15 14:25 by mait-all                   //
-//                        Updated: 2025/12/16 14:25 by mait-all                   //
+//                        Updated: 2025/12/17 10:55 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef PMERGEME_HPP
@@ -15,7 +15,10 @@
 # include <limits>
 # include <cstdlib>
 
+typedef std::pair<int, int> Pair;
+
 void	processInput(int ac, char **av);
 bool	isValidArg(std::string arg);
+void	mergeInsertionSort(std::vector<int>& input, int pairLevel);
 
 #endif /* __PMERGEME_HPP__ */
