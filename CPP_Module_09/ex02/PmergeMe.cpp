2e3481f3ab719b2bf9e701f59d04f4c9843a8200
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2025-12-22 11:50:54 +0100

    feat(CPP_09/ex02): implement the first two steps of merge-insertion algorithm:
    1 step: make pairs and separate them into large and small elements
    
    2 step: recursively sort large elements

--- CPP_Module_09/ex02/PmergeMe.cpp
+++ CPP_Module_09/ex02/PmergeMe.cpp
@@ -3,7 +3,7 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/12/15 14:25 by mait-all                   //
-//                        Updated: 2025/12/21 18:36 by mait-all                   //
+//                        Updated: 2025/12/22 11:47 by mait-all                   //
 // ****************************************************************************** //
 
 #include "PmergeMe.hpp"
@@ -44,6 +44,17 @@ int	Int::getValue()
 	return (_value);
 }
 
+void	Int::saveIndex(int idx)
+{
+	if (_c < 35)
+		_idxs[_c++] = idx; 
+}
+
+bool	Int::operator<(const Int& other)
+{
+	return (this->_value < other._value);
+}
+
 /**
  * todo...
  *
@@ -64,6 +75,65 @@ bool isValidArg(std::string arg)
 }
 
 
+void	makePairs(std::vector<Int>& input, std::vector<Int>& a, std::vector<Int>& b)
+{
+	int	n = input.size() - 1;
+	a.reserve(n / 2);
+	a.reserve(n / 2 + 1);
+
+	for (int i = 0; i < n; i += 2)
+	{
+		if (input[i] < input[i + 1])
+		{
+			input[i + 1].saveIndex(a.size());
+			a.push_back(input[i + 1]);
+			b.push_back(input[i]);
+		}
+		else
+		{
+			input[i].saveIndex(a.size());
+			a.push_back(input[i]);
+			b.push_back(input[i + 1]);
+		}
+	}
+	
+	// if there is an odd number add it to b
+	if ((n + 1) % 2 != 0)
+		b.push_back(input[n]);
+}
+
+void	mergeInsert(std::vector<Int>& input)
+{
+	// base case for recursion
+	if (input.size() < 2)
+		return ;
+
+	std::vector<Int>	a; // store larger nums
+	std::vector<Int>	b; // store small nums
+	std::vector<Int>	restoredB;
+
+	// step 1: make pairs and separate them into a (larger) and b (smaller)
+	makePairs(input, a, b);
+
+	// step 2: sort large elements recursively
+	mergeInsert(a);
+
+	std::cout << "a: ";
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		std::cout << a[i].getValue() << " ";
+	}
+	std::cout << "\nb: ";
+	for (size_t i = 0; i < b.size(); i++)
+	{
+		std::cout << b[i].getValue() << " ";
+	}
+
+	std::cout << "\n\n";
+	
+	
+}
+
 /**
  * todo...
  *
@@ -82,7 +152,7 @@ void processInput(int ac, char **av)
 		Int	nb(av[i]);
 		vect.push_back(nb);
 	}
-
+	mergeInsert(vect);
 	for (size_t i = 0; i < vect.size(); i++)
 	{
 		std::cout << vect[i].getValue() << " ";
--- CPP_Module_09/ex02/PmergeMe.hpp
+++ CPP_Module_09/ex02/PmergeMe.hpp
@@ -3,7 +3,7 @@
 //                        by: mait-all <mait-all@student.1337.ma>                 //
 //                                                                                //
 //                        Created: 2025/12/15 14:25 by mait-all                   //
-//                        Updated: 2025/12/21 17:44 by mait-all                   //
+//                        Updated: 2025/12/21 18:39 by mait-all                   //
 // ****************************************************************************** //
 
 #ifndef PMERGEME_HPP
@@ -50,5 +50,9 @@ class Int {
 // external helper functions
 void	processInput(int ac, char **av);
 bool	isValidArg(std::string arg);
+void	mergeInsert(std::vector<Int>& input);
+void	makePairs(std::vector<Int>& input,
+				  std::vector<Int>& a,
+				  std::vector<Int>& b);
 
 #endif /* __PMERGEME_HPP__ */
